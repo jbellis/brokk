67fd1eea974a1337ef896d419dbfb9bb87e551cd
Jonathan Ellis <jbellis@gmail.com>
2025-03-21 23:08:23 +0000

    demo files

--- src/main/java/io/github/jbellis/brokk/diffTool/BrokkDiffTool.java
+++ src/main/java/io/github/jbellis/brokk/diffTool/BrokkDiffTool.java
@@ -23,8 +23,8 @@ public class BrokkDiffTool
                 true,  // Enable file comparison mode
                 "", "", // Titles for the left and right content (empty in this case)
                 "", "", // Content for direct text comparison (not used here)
-                new File("C:\\Users\\Administrator\\Desktop\\java.txt"),  // Left sample file to compare
-                new File("C:\\Users\\Administrator\\Desktop\\world.txt")   // Right sample file to compare
+                new File("a.txt"),  // Left sample file to compare
+                new File("b.txt")   // Right sample file to compare
         );
         frame.add(brokkPanel);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
